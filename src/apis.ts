42fa287df20c1db92637c6c25093488e565dbb66
mateusbruno1 <mateusbruno294@gmail.com>
2021-06-09 14:28:24 -0300

    first commit

---
+++ src/apis.ts
@@ -0,0 +1,12 @@
+const config = [
+  {
+  url: 'https://backend.plenitudeigrejabatista.com.br',
+  route: 'EVENTS'
+  },
+  {
+  url: 'https://backend.plenitudeigrejabatista.com.br',
+  route: 'CELL'
+  }
+  ]
+  
+  export default config
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import * as express from 'express'
+import * as helmet from 'helmet'
+import apis from './apis'
+import * as httpProxy from 'express-http-proxy'
+
+const app = express()
+
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+apis.forEach(({ url, route }) => {
+app.use(`/${route}`, httpProxy(url, { timeout: 3000 }))
+})
+
+app.get('/', (req, res) => {
+return res.json('Application is runnig')
+})
+
+app.listen(3000, () => console.log('Application is running'))
